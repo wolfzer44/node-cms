7b5740f351ca5ad4e1887d6c1c23a3848fdbd1e7
Matheus Oliveira <mattoliveirabsb@gmail.com>
2016-07-04 15:51:07 -0300

    call new functions nad struture

--- app.js
+++ app.js
@@ -2,3 +2,15 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const routes = require('./routes/index');
+const middlewares = require('./middlewares/index');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/api/v1/', middlewares.verifyToken, routes.login);
+
+app.listen(3000, function() {
+  console.log('server running in port: 3000');
+})
